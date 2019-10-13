c6731181353f198ffc1abf6ed2b5568715872073
Amaury <dakub1984@gmail.com>
2019-10-13 17:49:04 +0200

    blog page

---
+++ pages/blog/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section id="posts">
+   <PostPreview 
+   v-for="post in posts"
+   :key="post.id"
+   :title="post.title"
+   :excerpt="post.previewText"
+   :thumbnailImage="post.thumbnailUrl"
+   :id="post.id"
+   />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Blog/PostPreview'
+export default {
+  components: {
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'title',
+          previewText: 'preview',
+          thumbnailUrl: 'https://static.mmzstatic.com/wp-content/uploads/2018/05/manquer-a-son-chat.jpg',
+          id: '1'
+        },
+        {
+          title: 'title dos',
+          previewText: 'preview dos',
+          thumbnailUrl: 'https://static.mmzstatic.com/wp-content/uploads/2018/05/manquer-a-son-chat.jpg',
+          id: '2'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
+</style>
---
+++ pages/contact/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="blog-page">
+    <h1>Sup'</h1>
+    <p>Contact me</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
